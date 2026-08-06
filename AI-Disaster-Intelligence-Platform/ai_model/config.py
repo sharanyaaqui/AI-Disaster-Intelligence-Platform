645853eb9d7250f1012258f7decd3e13cc4d54eb
Shaill11 <gshailja34@gmail.com>
2026-08-06 22:46:43 +0530

    Created deployment-ready AI module

--- AI-Disaster-Intelligence-Platform/ai_model/config.py
+++ AI-Disaster-Intelligence-Platform/ai_model/config.py
@@ -1,21 +1,12 @@
 IMAGE_SIZE = (224, 224)
 
-BATCH_SIZE = 32
-
-EPOCHS = 10
-
-NUM_CLASSES = 4
-
 CLASS_NAMES = [
     "cyclone",
     "earthquake",
     "flood",
     "wildfire"
 ]
-NUM_CLASSES = len(CLASS_NAMES)
-TRAIN_PATH = "ai_model/dataset/train"
-TEST_PATH = "ai_model/dataset/test"
-VALIDATION_PATH = "ai_model/dataset/validation"
+
 
 MODEL_PATH = "ai_model/models/disaster_classifier.keras"
 
